844fb2f48b9dd902aad4f4cc292d52fa02ae5dc3
Alice <zhangjiaying2000@hotmail.com>
2018-01-02 00:34:40 -0500

    Fixed an error related to calculating the x-int of linear equations

--- Tester.java
+++ Tester.java
@@ -3,10 +3,10 @@ import java.text.*;
 
 public class Tester
 {
-
-  public Tester()
-  {
-      //******************* Used to test the coef and coef2 random functions work and to see how they should work
+    
+    public Tester()
+    {
+        //******************* Used to test the coef and coef2 random functions work and to see how they should work
 //      double max = 0;
 //      DecimalFormat df3 = new DecimalFormat ("0.00");
 //      DecimalFormat df = new DecimalFormat("");
@@ -27,8 +27,8 @@ public class Tester
 //      //System.out.println(df.format(rand));
 //      }
 //      System.out.println("MAX: " + df.format(max));
-      
-      //*************************** Used to test the Math.random function
+        
+        //*************************** Used to test the Math.random function
 //      double a =0,b,c; 
 //      for(int x = 0;x<50;x++)
 //      {
@@ -42,10 +42,10 @@ public class Tester
 //      }
 //      a = Double.parseDouble(df.format(a));
 //      System.out.println(a);
-    //  System.out.println(Calculate.randCons(0));
-   //   System.out.println(Calculate.randCons(0));
-      
-      //************* Used to test all three random functions and displays the x-ints to show how the smaller positive one is above 100
+        //  System.out.println(Calculate.randCons(0));
+        //   System.out.println(Calculate.randCons(0));
+        
+        //************* Used to test all three random functions and displays the x-ints to show how the smaller positive one is above 100
 //      double cons = Calculate.randCons(0); 
 //      System.out.println(cons);
 //      double coef = Calculate.randCoef(0,cons);
@@ -58,55 +58,68 @@ public class Tester
 //      int x1=(int)((-coef+(Math.sqrt(Math.pow(coef,2)-4*coef2*cons)))/(2*coef2));
 //      int x2=(int)((-coef-(Math.sqrt(Math.pow(coef,2)-4*coef2*cons)))/(2*coef2));
 //      System.out.println("X1: " + x1 + " X2: " + x2);
-      
-      
+        
+        
 //******************** Actual program
-      int degree = 2;
-      double [] coeff = new double[degree + 1];
-      DecimalFormat maxDF = new DecimalFormat("#.###################");
-      DecimalFormat df = new DecimalFormat ("0.00");
-      
-      coeff[2]=Calculate.randCons(0);
-      coeff[1]=Calculate.randCoef(0,coeff[2]);
-      coeff[0]=Calculate.randCoef2(0,coeff[2],coeff[1]);
-      //Screen 1: Equation is introduced
-      String term2 = "", term3 = "";
-      if(coeff[1] != 0)
-          term2 = maxDF.format(coeff[1])+"x";
-      if(coeff[0] != 0)
-          term3 = maxDF.format(coeff[0])+"x^2 ";
-      System.out.println(maxDF.format(coeff[2]) + term2 + term3);//just for consur
-      //Screen 2: Introduce sales level
-      int x1=(int)((-coeff[1]+(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
-      int x2=(int)((-coeff[1]-(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
-      System.out.println("x1: " + x1 + " and x2: " + x2);
-      int sales = 0, x = 0;
-      if(x2>0 && (x2 < x1 || x1 < 0))
-          x = x2;
-      if(x1>0 && (x1 < x2 || x2 < 0))
-          x = x1;
-       sales = (int)(Math.random()*x);
-       System.out.println("x: " + x + " and Sales: " + sales);
-      //Screen 3: Calculate for selling price
-      double sell = 0;
-      for (int i = 0 ; i <= degree ; i++)
-      {
-          sell += coeff[i] * Math.pow(sales,degree-i);
-      }
-      sell = df.format(sell);
-      System.out.println("Sell price: $" + sell);
-      System.out.println("A) $" + sell); //Real answer
-      System.out.println("B) $" + df.format(Calculate.integrate(degree, coeff,0,sales, 0))); //Fake answer, forgot to subtract sell price
-      System.out.println("C) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sell)))); //Fake answer, used sell price as upper bounds
-      System.out.println("D) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sales)))); //Fake answer, used sell price as upper bounds and subtracted sales level
- 
-      //Screen 4: Calculate integral
-      System.out.println("A) $" + df.format(Calculate.integrate(degree, coeff,0,sales, sell))); //Real answer
-      System.out.println("B) $" + df.format(Calculate.integrate(degree, coeff,0,sales, 0))); //Fake answer, forgot to subtract sell price
-      System.out.println("C) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sell)))); //Fake answer, used sell price as upper bounds
-      System.out.println("D) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sales)))); //Fake answer, used sell price as upper bounds and subtracted sales level
- 
-      
+        int degree = 2;
+        double [] coeff = new double[degree + 1];
+        DecimalFormat maxDF = new DecimalFormat("#.###################");
+        DecimalFormat df = new DecimalFormat ("0.00");
+        String term2 = "", term3 = "";
+        int sales = 0, x = 0;
+        
+        coeff[2]=Calculate.randCons(0);
+        coeff[1]=Calculate.randCoef(0,coeff[2]);
+        coeff[0]=Calculate.randCoef2(0,coeff[2],coeff[1]);
+        //Screen 1: Equation is introduced
+        if(coeff[1] != 0)
+            term2 = maxDF.format(coeff[1])+"x";
+        if(coeff[0] != 0)
+            term3 = maxDF.format(coeff[0])+"x^2 ";
+        System.out.println(maxDF.format(coeff[2]) + term2 + term3);//just for consur
+        //Screen 2: Introduce sales level
+        if(coeff[0] != 0)
+        {
+            int x1=(int)((-coeff[1]+(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
+            int x2=(int)((-coeff[1]-(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
+            System.out.println("x1: " + x1 + " and x2: " + x2);
+            if(x2>0 && (x2 < x1 || x1 < 0))
+                x = x2;
+            if(x1>0 && (x1 < x2 || x2 < 0))
+                x = x1;
+        }
+        else
+            x = (int)(-coeff[2]/coeff[1]);
+        sales = (int)(Math.random()*x);
+        System.out.println("x: " + x + " and Sales: " + sales);
+        
+        //Screen 3: Calculate for selling price
+        double sell = 0;
+        for (int i = 0 ; i <= degree ; i++)
+        {
+            sell += coeff[i] * Math.pow(sales,degree-i);
+        }
+        sell = Double.parseDouble(df.format(sell));
+        System.out.println("A) $" + df.format(sell)); //Real answer
+        System.out.println("B) $" + df.format(Math.abs(sell - coeff[2]))); //Fake answer, didn't add constant
+        if(-coeff[0]>0.001)
+            System.out.println("C) $" + df.format(sell - coeff[0]*Math.pow(sales,2))); //Fake answer, didn't add Ax^2
+        else if(-coeff[1]>0.001)
+            System.out.println("C) $" + df.format(sell - coeff[1]*sales)); //Fake answer, didn't add Bx
+        else
+            System.out.println("C) $" + df.format(sell - 1)); //Fake answer, in case, by some magical chance, both B and C are super small and sell price is close to constant
+        if((int)(Math.random()*2)==0)
+            System.out.println("D) $" + df.format(sell/2)); //Fake answer, divided sell by two
+        else
+            System.out.println("D) $" + df.format(sell*2)); //Fake answer, multiplied sell by two
+        
+        //Screen 4: Calculate integral
+        System.out.println("A) $" + df.format(Calculate.integrate(degree, coeff,0,sales, sell))); //Real answer
+        System.out.println("B) $" + df.format(Calculate.integrate(degree, coeff,0,sales, 0))); //Fake answer, forgot to subtract sell price
+        System.out.println("C) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sell)))); //Fake answer, used sell price as upper bounds
+        System.out.println("D) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sales)))); //Fake answer, used sell price as upper bounds and subtracted sales level
+        
+        
 // ********************* To test the randomness of Math.random
 //      int one = 0, zero = 0, ack = 0,what = 0;
 //      for(int x = 0;x<100;x++)
@@ -120,10 +133,10 @@ public class Tester
 //              what++;
 //      }
 //  System.out.println("Zeros: " + zero + " Ones: " + one + " Whats: " + what);
-  }
- 
-  public static void main(String [] args)
-  {
-    new Tester();
-  }  
+    }
+    
+    public static void main(String [] args)
+    {
+        new Tester();
+    }  
 }
