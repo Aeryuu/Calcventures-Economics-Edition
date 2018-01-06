359ddd7bc571dd2e44fb5778b8f0767afb08d951
Alice <zhangjiaying2000@hotmail.com>
2018-01-05 23:25:47 -0500

    Started coding the logic for product surplus

--- Tester.java
+++ Tester.java
@@ -60,7 +60,7 @@ public class Tester
 //      System.out.println("X1: " + x1 + " X2: " + x2);
         
         
-//******************** Actual program for consur
+//******************** Actual program for prosur
         int degree = 2;
         double [] coeff = new double[degree + 1];
         DecimalFormat maxDF = new DecimalFormat("#.###################");
@@ -78,19 +78,7 @@ public class Tester
             term3 = maxDF.format(coeff[0])+"x^2 "; //Ax^2 form
         System.out.println(maxDF.format(coeff[2]) + term2 + term3);//just for consur //just for testing
         //Screen 2: Introduce sales level
-        if(coeff[0] != 0)
-        {
-            int x1=(int)((-coeff[1]+(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
-            int x2=(int)((-coeff[1]-(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
-            System.out.println("x1: " + x1 + " and x2: " + x2); //used for testing
-            if(x2>0 && (x2 < x1 || x1 < 0))
-                x = x2;
-            if(x1>0 && (x1 < x2 || x2 < 0))
-                x = x1;
-        }
-        else
-            x = (int)(-coeff[2]/coeff[1]);
-        sales = (int)(Math.random()*x);
+        sales = (int)(Math.random()*10000);
         System.out.println("x: " + x + " and Sales: " + sales); //partially used for testing
         
         //Screen 3: Calculate for selling price
@@ -146,7 +134,6 @@ public class Tester
         
         
         
-        
 // ********************* To test the randomness of Math.random
 //      int one = 0, zero = 0, ack = 0,what = 0;
 //      for(int x = 0;x<100;x++)
@@ -162,8 +149,83 @@ public class Tester
 //  System.out.println("Zeros: " + zero + " Ones: " + one + " Whats: " + what);
     }
     
+    //All the logic for consumer surplus
+    public void consumerSurplus() 
+    {
+        int degree = 2;
+        double [] coeff = new double[degree + 1];
+        DecimalFormat maxDF = new DecimalFormat("#.###################");
+        DecimalFormat df = new DecimalFormat ("0.00");
+        String term2 = "", term3 = "";
+        int sales = 0, x = 0;
+        
+        coeff[2]=Calculate.randCons(0);
+        coeff[1]=Calculate.randCoef(0,coeff[2]);
+        coeff[0]=Calculate.randCoef2(0,coeff[2],coeff[1]);
+        
+        //Screen 1: Equation is introduced
+        if(coeff[1] != 0)
+            term2 = maxDF.format(coeff[1])+"x"; //Bx form
+        if(coeff[0] != 0)
+            term3 = maxDF.format(coeff[0])+"x^2 "; //Ax^2 form
+        System.out.println(maxDF.format(coeff[2]) + term2 + term3);//just for consur //just for testing
+        
+        //Screen 2: Introduce sales level
+        if(coeff[0] != 0)
+        {
+            int x1=(int)((-coeff[1]+(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
+            int x2=(int)((-coeff[1]-(Math.sqrt(Math.pow(coeff[1],2)-4*coeff[0]*coeff[2])))/(2*coeff[0]));
+            System.out.println("x1: " + x1 + " and x2: " + x2); //used for testing
+            if(x2>0 && (x2 < x1 || x1 < 0))
+                x = x2;
+            if(x1>0 && (x1 < x2 || x2 < 0))
+                x = x1;
+        }
+        else
+            x = (int)(-coeff[2]/coeff[1]);
+        sales = (int)(Math.random()*x);
+        System.out.println("x: " + x + " and Sales: " + sales); //partially used for testing
+        
+        //Screen 3: Calculate for selling price
+        double sell = 0;
+        for (int i = 0 ; i <= degree ; i++)
+        {
+            sell += coeff[i] * Math.pow(sales,degree-i);
+        }
+        sell = Double.parseDouble(df.format(sell)); //sell becomes the rounded price displayed on screen
+        ArrayList<String> arr = new ArrayList<String>(Arrays.asList("A) $" + df.format(sell),"B) $" + df.format(Math.abs(sell - coeff[2]))));
+        if(-coeff[0]>0.001)
+            arr.add("C) $" + df.format(sell - coeff[0]*Math.pow(sales,2))); //Fake answer, didn't add Ax^2
+        else if(-coeff[1]>0.001)
+            arr.add("C) $" + df.format(sell - coeff[1]*sales)); //Fake answer, didn't add Bx
+        else
+            arr.add("C) $" + df.format(sell - 1)); //Fake answer, in case, by some magical chance, both B and C are super small and sell price is close to constant
+        if((int)(Math.random()*2)==0)
+            arr.add("D) $" + df.format(sell/2)); //Fake answer, divided sell by two
+        else
+            arr.add("D) $" + df.format(sell*2)); //Fake answer, multiplied sell by two 
+        int choice;
+        for (int y = 4; y >= 1; y--)
+        {
+            choice = (int)(Math.random()*y);
+            System.out.println("Choice: " + choice + " Y: " + y + " Array int" + arr.get(choice)); //used for testing
+            System.out.println(arr.get(choice));
+            arr.remove(choice);
+        }
+        //Screen 4: Calculate integral
+        arr = new ArrayList<String>(Arrays.asList("A) $" + df.format(Calculate.integrate(degree, coeff,0,sales, sell)),"B) $" + df.format(Calculate.integrate(degree, coeff,0,sales, 0)),"C) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sell))),"D) $" + df.format(Math.abs(Calculate.integrate(degree, coeff,0,sell, sales)))));
+        for (int y = 4; y >= 1; y--)
+        {
+            choice = (int)(Math.random()*y);
+            System.out.println("Choice: " + choice + " Y: " + y + " Array int" + arr.get(choice)); //used for testing
+            System.out.println(arr.get(choice));
+            arr.remove(choice);
+        }
+    }
+    
     public static void main(String [] args)
     {
-        new Tester();
+        Tester t = new Tester();
+        t.consumerSurplus();
     }  
 }
