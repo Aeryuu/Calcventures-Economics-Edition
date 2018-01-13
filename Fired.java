bc9230963361c01f4720f0b139a2b2e208ee8087
Tiffany Tran <tiffany.tran285@gmail.com>
2018-01-13 13:39:28 -0500

    Basic fired class (not done)

---
+++ Fired.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Fired extends JPanel
+{ 
+  public Fired()
+  {        
+    addMouseListener(new MouseAdapter() { 
+      public void mousePressed(MouseEvent e) { 
+        
+        int relativeX = e.getX();
+        int relativeY = e.getY();
+        
+        if (relativeX >= 130 && relativeX <= 215 && relativeY >= 500 && relativeY <= 553) //Go back to main menu
+        {
+            Driver.changeScreens("MainMenu");
+        }
+        else if (relativeX >= 130 && relativeX <= 215 && relativeY >= 500 && relativeY <= 553) //Go back to adventure choosing screen
+        {
+            Driver.changeScreens("AdventureSelect");
+        }
+      } 
+    }); 
+  }
+  
+  public void paintComponent (Graphics g)
+  {
+    MediaTracker tracker = new MediaTracker (new Frame ());    
+    Image fired = Toolkit.getDefaultToolkit ().getImage ("Backgrounds/Fired.png");
+    tracker.addImage (fired, 0); 
+    g.drawImage (fired, 0, 0, this);
+    //put picture of character and write sentence with name
+  }
+}
